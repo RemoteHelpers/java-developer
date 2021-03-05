dcf8ff67b1854cdd0b1a70ebf348caa0a6f73d21
RemoteHelpers <49642398+RemoteHelpers@users.noreply.github.com>
2021-03-05 18:36:10 +0300

    Animated elements

--- main.js
+++ main.js
@@ -2,8 +2,38 @@ const mobileMenuBtn = document.querySelector(".mobile-menu-btn");
 const mobileMenuCloseBtn = document.querySelector(".mobile-menu-close-btn");
 const mobileMenu = document.querySelector(".mobile-menu");
 const body = document.querySelector("*");
+const inNumberItem = document.querySelectorAll(".numbers-content");
+// const inNumbersSection = document.querySelector("#in_numbers");
+const advItem = document.querySelectorAll(".advatages-item");
+const requirementItem = document.querySelectorAll(".requirement");
 
 
+
+for (let i = 0; i < inNumberItem.length; ++i) {
+    inNumberItem[i].addEventListener('mouseenter', scaledDiv);
+ }
+
+ for (let i = 0; i < advItem.length; ++i) {
+    advItem[i].addEventListener('mouseenter', getHigh);
+ }
+
+ for (let i = 0; i < requirementItem.length; ++i) {
+    requirementItem[i].addEventListener('mouseenter', getHigh);
+ }
+
+
+
+
+
+ function scaledDiv (elem) {
+    this.classList.add("scaled");
+    setTimeout(() => this.classList.remove("scaled"), 1000);
+ }
+
+ function getHigh (elem) {
+    this.classList.add("higher");
+    setTimeout(() => this.classList.remove("higher"), 500);
+ }
 
 function openMenu () {
     mobileMenuBtn.addEventListener('click', function(){
@@ -30,6 +60,5 @@ function closeMobileMenu() {
     body.classList.remove("restrict-y");
     openMenu();
 }
-
 
 openMenu ();
